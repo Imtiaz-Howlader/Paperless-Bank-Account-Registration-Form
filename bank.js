e65159d101b4273cae645e9697099d664b4756a9
Md Imtiaz Howlader <142086387+Imtiaz-Howlader@users.noreply.github.com>
2026-05-20 01:28:47 +0600

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,15 @@
+function goToStep1() {
+    window.location.href = "step1.html";
+}
+
+function goToStep2() {
+    window.location.href = "step2.html";
+}
+
+function goToStep3() {
+    window.location.href = "step3.html";
+}
+
+function goToStep4() {
+    window.location.href = "step4.html";
+}
